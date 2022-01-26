cc0b8ca838723c5b1f477ded2512aca904c9b366
Tan Yan Lyn <e0424594@u.nus.edu>
2022-01-26 21:58:45 +0800

    add typedefs

---
+++ Team32/Code32/src/spa/src/Common/Utils.h
@@ -0,0 +1,8 @@
+//
+// Created by Yanzieee on 26/1/2022.
+//
+
+#ifndef INC_21S2_CP_SPA_TEAM_32_UTILS_H
+#define INC_21S2_CP_SPA_TEAM_32_UTILS_H
+
+#endif //INC_21S2_CP_SPA_TEAM_32_UTILS_H
---
+++ Team32/Code32/src/spa/src/Common/typeDefs.h
@@ -0,0 +1,49 @@
+//
+// Created by Yanzieee on 26/1/2022.
+//
+
+#ifndef INC_21S2_CP_SPA_TEAM_32_UTIL_H
+#define INC_21S2_CP_SPA_TEAM_32_UTIL_H
+
+#endif //INC_21S2_CP_SPA_TEAM_32_UTIL_H
+
+#include <string>
+
+using namespace std;
+
+#ifndef INC_21S2_CP_SPA_TEAM_32_UTIL_H
+#define INC_21S2_CP_SPA_TEAM_32_UTIL_H
+
+#endif //INC_21S2_CP_SPA_TEAM_32_UTIL_H
+
+typedef int StmtRef;
+typedef std::string VarRef;
+typedef std::string ProcRef;
+
+enum StmtType { assign, print, call, read, whileStmt, ifStmt, standard };
+
+typedef struct StmtInfo {
+    StmtRef reference;
+    StmtType type;
+}
+
+typedef std::vector<StmtInfo> StmtInfoList;
+typedef std::vector<VarRef> VarRefList;
+
+typedef struct Synonym {
+    QueryType type;
+    std::string symbol;
+} Synonym;
+
+typedef struct SuchThatClause {
+    Relationship relation;
+    Pair<Synonym, Pair<StmtRef, VarRef> lhs;
+} SuchThatClause;
+
+typedef struct PatternClause {
+    Synonym lhs;
+    // Some kind of math expression?
+} PatternClause;
+
+
+
