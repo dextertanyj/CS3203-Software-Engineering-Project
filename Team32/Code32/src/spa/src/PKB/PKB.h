f07a015eb45c85560880342fb83423c37ac8a97b
Tan Yan Lyn <e0424594@u.nus.edu>
2022-01-26 21:45:49 +0800

    resolved some issues

--- Team32/Code32/src/spa/src/PKB/PKB.h
+++ Team32/Code32/src/spa/src/PKB/PKB.h
@@ -1,3 +1,14 @@
+#pragma once
+
+#include<stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include "ParentStore.h"
+#include "FollowStore.h"
+#include "UseStore.h"
+#include "ModifyStore.h"
 
 using namespace std;
 typedef short PROC;
@@ -6,10 +17,47 @@ class TNode;
 
 class VarTable;  // no need to #include "VarTable.h" as all I need is pointer
 
+enum stmtType { assign, print, call, read, whileStmt, ifStmt, standard };
+
 class PKB {
 public:
+    /*
 	static VarTable* varTable; 
 	static int setProcToAST(PROC p, TNode* r);
 	static TNode* getRootAST (PROC p);
+     */
+    PKB();
 
+    // Set methods called by Source processor
+    bool setFollows(int stmtNo1, int stmtNo2);
+    bool setParent(int stmtNo1, int stmtNo2);
+    void setProc(std::string proc_name, vector<int> idxList);
+    void setStmtType(int stmtNo, stmtType type);
+    void setUses(int stmtNo, std::string var_name);
+    void setModifies(int stmtNo, std::string var_name);
+
+    // Get methods called by PQL
+    // Parent get methods
+    int getParent(int stmtNo);
+    unordered_set<int> getChildren(int stmtNo);
+    bool checkParents(int stmtNo1, int stmtNo2);
+
+    // Follow get methods
+    int getFollowee(int stmtNo);
+    int getFollower(int stmtNo);
+    bool checkFollows(int stmtNo1, int stmtNo2);
+
+    // Others
+    void clear();
+    void setNumStatements(int size);
+    void populateComplexRelations();
+
+private:
+    static ParentStore parentStore;
+    static FollowStore followStore;
+    static UseStore useStore;
+    static ModifyStore modifyStore;
+    bool checkInvalidStmts(int stmtNo1, int stmtNo2);
+    static std::unordered_map<std::string, vector<int>> procMap;
+    static std::unordered_map<int, stmtType> typeMap;
 };
