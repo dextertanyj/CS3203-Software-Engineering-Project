351417f5afd7ce2eaefaecbc0ab3a150ae10af4c
ypinhsuan <yenpinhsuan@gmail.com>
2022-01-27 11:00:48 +0800

    Implement QueryResult

--- Team32/Code32/src/spa/src/QP/QueryResult.h
+++ Team32/Code32/src/spa/src/QP/QueryResult.h
@@ -1,3 +1,15 @@
-class QueryResult {
+#ifndef QUERY_RESULT_H
+#define QUERY_RESULT_H
 
+#include "Common/TypeDefs.h"
+
+class QueryResult {
+public:
+	QueryResult();
+	QueryResult(StmtInfoList stmtList, VarRefList varList);
+
+	StmtInfoList stmtList;
+	VarRefList varList;
 };
+
+#endif /* QUERY_RESULT_H */
