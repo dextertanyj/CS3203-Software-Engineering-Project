c09c57ac6e82d202de002b1f45c80d5dcc57ed65
ypinhsuan <yenpinhsuan@gmail.com>
2022-01-27 20:20:53 +0800

    Use smart pointers

--- Team32/Code32/src/spa/src/QP/QueryEvaluator.h
+++ Team32/Code32/src/spa/src/QP/QueryEvaluator.h
@@ -1,12 +1,16 @@
 #ifndef QUERY_EVALUATOR_H_
 #define QUERY_EVALUATOR_H_
 
+#include <memory>
+
+using namespace std;
+
 class QueryProperties;
 class QueryResult;
 
 class QueryEvaluator {
 public:
-	static QueryResult* executeQuery(QueryProperties* queryProperties);
+	static unique_ptr<QueryResult> executeQuery(QueryProperties* queryProperties);
 };
 
 #endif // QUERY_EVALUATOR_H_
