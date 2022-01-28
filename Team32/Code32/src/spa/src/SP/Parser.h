53cc375224b21f5290f4ca71d170c8d7ef64aae9
Dexter Tan <dev@dextertanyj.com>
2022-01-28 20:18:32 +0800

    Reorganize parser header declarations

--- Team32/Code32/src/spa/src/SP/Parser.h
+++ Team32/Code32/src/spa/src/SP/Parser.h
@@ -31,16 +31,16 @@ private:
 	Lexer lex;
 	size_t statement_count;
 
-	unique_ptr<ArithmeticExpressionNode> parseArithmeticExpression();
-	unique_ptr<AssignmentNode> parseAssignmentStatement();
-	unique_ptr<ConditionalExpressionNode> parseConditionalExpression();
-	unique_ptr<ConstantNode> parseConstant();
-	unique_ptr<IfNode> parseIfStatement();
-	unique_ptr<ProcedureNode> parseProcedure();
 	unique_ptr<ProgramNode> parseProgram();
-	unique_ptr<RelationalExpressionNode> parseRelationalExpression();
+	unique_ptr<ProcedureNode> parseProcedure();
 	unique_ptr<StatementListNode> parseStatementList();
 	unique_ptr<StatementNode> parseStatement();
-	unique_ptr<VariableNode> parseVariable();
+	unique_ptr<AssignmentNode> parseAssignmentStatement();
+	unique_ptr<IfNode> parseIfStatement();
 	unique_ptr<WhileNode> parseWhileStatement();
+	unique_ptr<ConditionalExpressionNode> parseConditionalExpression();
+	unique_ptr<RelationalExpressionNode> parseRelationalExpression();
+	unique_ptr<ArithmeticExpressionNode> parseArithmeticExpression();
+	unique_ptr<VariableNode> parseVariable();
+	unique_ptr<ConstantNode> parseConstant();
 };
