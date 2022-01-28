9d6c741aae16ba87963127fa34157bf5522c1379
Dexter Tan <dev@dextertanyj.com>
2022-01-28 20:11:42 +0800

    Add declarations for parser subroutines

--- Team32/Code32/src/spa/src/SP/Parser.h
+++ Team32/Code32/src/spa/src/SP/Parser.h
@@ -1,3 +1,46 @@
 #pragma once
+
+#include <string>
+
+#include "SP/AssignmentNode.h"
+#include "SP/ConditionalExpressionNode.h"
+#include "SP/ConstantNode.h"
+#include "SP/IfNode.h"
+#include "SP/Lexer.h"
+#include "SP/ProcedureNode.h"
+#include "SP/ProgramNode.h"
+#include "SP/RelationalExpressionNode.h"
+#include "SP/StatementListNode.h"
+#include "SP/StatementNode.h"
+#include "SP/VariableNode.h"
+#include "SP/WhileNode.h"
+
+
 using namespace std;
-int Parse();
+
+struct ParseException : public runtime_error {
+	using runtime_error::runtime_error;
+};
+
+class Parser {
+public:
+	Parser();
+	bool parse(string source);
+
+private:
+	Lexer lex;
+	size_t statement_count;
+
+	unique_ptr<ArithmeticExpressionNode> parseArithmeticExpression();
+	unique_ptr<AssignmentNode> parseAssignmentStatement();
+	unique_ptr<ConditionalExpressionNode> parseConditionalExpression();
+	unique_ptr<ConstantNode> parseConstant();
+	unique_ptr<IfNode> parseIfStatement();
+	unique_ptr<ProcedureNode> parseProcedure();
+	unique_ptr<ProgramNode> parseProgram();
+	unique_ptr<RelationalExpressionNode> parseRelationalExpression();
+	unique_ptr<StatementListNode> parseStatementList();
+	unique_ptr<StatementNode> parseStatement();
+	unique_ptr<VariableNode> parseVariable();
+	unique_ptr<WhileNode> parseWhileStatement();
+};
