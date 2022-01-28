4fbe4c95b211e1d3461d69940550ec4b4adc0275
Dexter Tan <dev@dextertanyj.com>
2022-01-28 20:16:12 +0800

    Implement program, procedure, and statement list parsers

--- Team32/Code32/src/spa/src/SP/Parser.cpp
+++ Team32/Code32/src/spa/src/SP/Parser.cpp
@@ -1,14 +1,50 @@
-#include<stdio.h>
-#include <iostream>
-#include <string>
-#include <vector>
+#include "SP/Converter.h"
+#include "SP/Parser.h"
+#include "SP/Validator.h"
 
 using namespace std;
 
-#include "PKB/PKB.h"
-#include "Common/TNode.h"
+Parser::Parser() = default;
 
-int Parse () {
+bool Parser::parse(string source) {
+	try {
+		this->lex.initialize(std::move(source));
+	} catch (TokenizationException&) {
+		return false;
+	}
+	this->statement_count = 1;
+	try {
+		unique_ptr<ProgramNode> program = this->parseProgram();
+	} catch (ParseException&) {
+		return false;
+	}
+	return true;
+}
 
-	return 0;
+unique_ptr<ProcedureNode> Parser::parseProcedure() {
+	this->lex.next_if("procedure");
+	ProcRef name = lex.read_token();
+	if (!Validator::validateName(name)) {
+		throw ParseException("Invalid procedure name");
+	}
+	this->lex.next_if("{");
+	unique_ptr<StatementListNode> statement_list = this->parseStatementList();
+	this->lex.next_if("}");
+	return make_unique<ProcedureNode>(name, std::move(statement_list));
+}
+
+unique_ptr<ProgramNode> Parser::parseProgram() {
+	unique_ptr<ProgramNode> program = make_unique<ProgramNode>();
+	do {
+		program->addProcedureNode(this->parseProcedure());
+	} while (!this->lex.peek_token().empty());
+	return program;
+}
+
+unique_ptr<StatementListNode> Parser::parseStatementList() {
+	unique_ptr<StatementListNode> statement_list = make_unique<StatementListNode>();
+	do {
+		statement_list->addStatementNode(this->parseStatement());
+	} while (this->lex.peek_token() != "}");
+	return statement_list;
 }
