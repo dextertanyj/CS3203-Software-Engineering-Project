f9996f3adcf06745220efe196ce4307849531eb6
Wei Kit <weikit89@gmail.com>
2022-02-03 19:35:02 +0800

    Create Follow Test and test case for a basic set and get.

---
+++ Team32/Code32/src/unit_testing/src/PKB/FollowTest.cpp
@@ -0,0 +1,35 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "../../../spa/src/PKB/FollowStore.cpp"
+#include "../../../spa/src/Common/TypeDefs.h"
+
+// TEST_CASE("Set Follows") {
+  
+//   StmtRef s1 = 1;
+//   StmtRef s2 = 2;
+//   StmtRef s3 = 3;
+  
+// }
+
+TEST_CASE( "Basic Follows Set and Get") {
+  StmtRef s1 = 1;
+  StmtRef s2 = 2;
+  StmtRef s3 = 3;
+  FollowStore fs = FollowStore();
+  
+  fs.setFollows(s1, s2);
+  fs.setFollows(s2, s3);
+
+  // Ensure simple follow relation is stored successfully.
+  CHECK( fs.checkFollows(s1, s2));
+  CHECK( fs.checkFollows(s2, s3));
+
+  // Ensure Follows* behavior does not appear for simple follow.
+  CHECK_FALSE( fs.checkFollows(s1, s3));
+
+  CHECK_FALSE(fs.checkFollows(s1, s1));
+  CHECK_FALSE(fs.checkFollows(s2, s1));
+  CHECK_FALSE(fs.checkFollows(s3, s2));
+  
+}
