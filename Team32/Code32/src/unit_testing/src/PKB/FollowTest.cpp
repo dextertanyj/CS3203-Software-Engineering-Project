be4cc27a7eb98b7f72bf49a721cde2b113895fee
Wei Kit <weikit89@gmail.com>
2022-02-11 00:29:33 +0800

    Refine Follow Test Cases

--- Team32/Code32/src/unit_testing/src/PKB/FollowTest.cpp
+++ Team32/Code32/src/unit_testing/src/PKB/FollowTest.cpp
@@ -1,5 +1,6 @@
 #include "Common/TypeDefs.h"
 #include "PKB/PKB.h"
+#include "PKBUtils.cpp"
 #include "catch.hpp"
 #include "memory"
 
@@ -8,33 +9,31 @@
  */
 
 TEST_CASE("Follows Methods") {
-	PKB pkb = PKB();
+	PKB pkb = generateFollowTestPKB();
 	StmtRef s1 = 1;
 	StmtRef s2 = 2;
 	StmtRef s3 = 3;
 	StmtRef sIntMax = INT32_MAX;
 	StmtRef sMinusOne = -1;
-	CHECK_NOTHROW(pkb.setStmtType(1, StmtType::Assign));
-	CHECK_NOTHROW(pkb.setStmtType(2, StmtType::IfStmt));
-	CHECK_NOTHROW(pkb.setStmtType(3, StmtType::Print));
-	CHECK_NOTHROW(pkb.setStmtType(INT32_MAX, StmtType::Read));
-	CHECK_THROWS(pkb.setStmtType(-1, StmtType::Assign));
 
 	SECTION("Check validity of arguments") {
+		// Invalid setting of negative statement number to statement map.
+		CHECK_THROWS(pkb.setStmtType(-1, StmtType::Assign));
+
 		// Verify that normal setting works.
 		CHECK_NOTHROW(pkb.setFollows(s1, s2));
 		CHECK_NOTHROW(pkb.setFollows(s2, s3));
-		pkb.clear();
+		pkb = generateFollowTestPKB();
 
 		// If s1 already has a follower, it should not be able to have a new direct follower.
 		CHECK_NOTHROW(pkb.setFollows(s1, s2));
 		CHECK_THROWS(pkb.setFollows(s1, sIntMax));
-		pkb.clear();
+		pkb = generateFollowTestPKB();
 
 		// If s3 already follows s2, then it should be allowed to follow s1.
 		CHECK_NOTHROW(pkb.setFollows(s2, s3));
 		CHECK_THROWS(pkb.setFollows(s1, s3));
-		pkb.clear();
+		pkb = generateFollowTestPKB();
 
 		// Verify that improper arguments lead to an exception thrown.
 		CHECK_THROWS(pkb.setFollows(sMinusOne, s1));
@@ -59,10 +58,10 @@ TEST_CASE("Follows Methods") {
 		// Ensure Follows* behavior does not appear for simple follow.
 		CHECK_FALSE(pkb.checkFollows(s1, s3));
 
+		// More negative cases.
 		CHECK_FALSE(pkb.checkFollows(s1, s1));
 		CHECK_FALSE(pkb.checkFollows(s2, s1));
 		CHECK_FALSE(pkb.checkFollows(s3, s2));
-		pkb.clear();
 	}
 }
 
