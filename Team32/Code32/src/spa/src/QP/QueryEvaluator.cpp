6f9fb1a26e27ee16ff7b04089fafbcef9ca6bb31
Dexter Tan <dev@dextertanyj.com>
2022-02-10 23:09:21 +0800

    Update typename

--- Team32/Code32/src/spa/src/QP/QueryEvaluator.cpp
+++ Team32/Code32/src/spa/src/QP/QueryEvaluator.cpp
@@ -9,7 +9,7 @@ QueryResult QueryEvaluator::executeQuery(QueryProperties& queryProperties) {
 	if (suchThatClauseList.empty() && patternClauseList.empty()) {
 		Declaration select = queryProperties.getSelect();
 		if (select.type == DesignEntity::stmt) {
-			StmtInfoPtrList stmt_pointers = pkb.getStatements();
+			StmtInfoList stmt_pointers = pkb.getStatements();
 			vector<StmtRef> stmts;
 			for (const shared_ptr<StmtInfo>& stmt : stmt_pointers) {
 				stmts.push_back(stmt->reference);
@@ -24,7 +24,7 @@ QueryResult QueryEvaluator::executeQuery(QueryProperties& queryProperties) {
 	return evaluateSuchThatClauses(suchThatClauseList);
 }
 QueryResult QueryEvaluator::evaluateSuchThatClauses(SuchThatClauseList& suchThatClauseList) {
-	StmtInfoPtrList stmt_pointers = pkb.getStatements();
+	StmtInfoList stmt_pointers = pkb.getStatements();
 	vector<StmtRef> stmts;
 	for (const shared_ptr<StmtInfo>& stmt : stmt_pointers) {
 		stmts.push_back(stmt->reference);
