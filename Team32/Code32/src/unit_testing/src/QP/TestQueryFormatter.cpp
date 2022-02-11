8c6771a5aa97eba1acd6e7e04dbc31ccb6be2357
ypinhsuan <yenpinhsuan@gmail.com>
2022-02-11 16:32:39 +0800

    Fix QueryFormatterTest

--- Team32/Code32/src/unit_testing/src/QP/TestQueryFormatter.cpp
+++ Team32/Code32/src/unit_testing/src/QP/TestQueryFormatter.cpp
@@ -12,5 +12,10 @@ TEST_CASE("QP::QueryFormatter::formatQuery List of statement number") {
 	QueryProperties properties = QueryProperties({}, { DesignEntity::stmt, "s" }, {}, {});
 	QueryResult result = QueryResult();
 	result.addColumn("s", { "1", "2", "3", "3"});
-	REQUIRE(QueryFormatter::formatResult(properties, result) == vector<string>({"1", "2", "3"}));
+	result.addColumn("sss", { "1", "2", "3", "4"});
+
+	vector<string>resultString = QueryFormatter::formatResult(properties, result);
+	
+	sort(resultString.begin(), resultString.end());
+	REQUIRE(resultString == vector<string>({"1", "2", "3"}));
 }
