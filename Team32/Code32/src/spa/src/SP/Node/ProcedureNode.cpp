c5a0f4e79c6db3c09ed2c437fe8cb8abc775b2a2
samlsm <samlsm2803@gmail.com>
2022-02-12 11:28:21 +0800

    Add code for parsing procedure

--- Team32/Code32/src/spa/src/SP/Node/ProcedureNode.cpp
+++ Team32/Code32/src/spa/src/SP/Node/ProcedureNode.cpp
@@ -4,7 +4,8 @@
 
 using namespace std;
 
-SP::Node::ProcedureNode::ProcedureNode(string name, unique_ptr<StatementListNode> stmtLst) : name(move(name)), stmtLst(move(stmtLst)) {}
+SP::Node::ProcedureNode::ProcedureNode(string name, unique_ptr<StatementListNode> stmtLst, StmtRef start, StmtRef end)
+    : name(move(name)), stmtLst(move(stmtLst)), start(start), end(end) {}
 
 unique_ptr<SP::Node::ProcedureNode> SP::Node::ProcedureNode::parseProcedure(Lexer& lex, int& statement_count) {
 	lex.nextIf("procedure");
@@ -13,9 +14,11 @@ unique_ptr<SP::Node::ProcedureNode> SP::Node::ProcedureNode::parseProcedure(Lexe
 		throw SP::ParseException("Invalid procedure name");
 	}
 	lex.nextIf("{");
+    StmtRef start = statement_count;
 	unique_ptr<StatementListNode> statement_list = StatementListNode::parseStatementList(lex, statement_count);
+    StmtRef end = statement_count - 1;
 	lex.nextIf("}");
-	return make_unique<ProcedureNode>(name, std::move(statement_list));
+	return make_unique<ProcedureNode>(name, std::move(statement_list), start, end);
 }
 
 bool SP::Node::ProcedureNode::extract(PKB& pkb) {
@@ -23,6 +26,7 @@ bool SP::Node::ProcedureNode::extract(PKB& pkb) {
 	return true;
 }
 
-bool SP::Node::ProcedureNode::equals(shared_ptr<ProcedureNode> object) {
-	return this->name == object->name && this->stmtLst->equals(object->stmtLst);
+bool SP::Node::ProcedureNode::equals(shared_ptr<ProcedureNode>& object) {
+	return this->name == object->name && this->stmtLst->equals(object->stmtLst) && this->start == object->start
+        && this->end == object->end;
 }
