4ba8110de60a848c4456237822351a4f1d9b098b
ypinhsuan <yenpinhsuan@gmail.com>
2022-02-19 13:12:47 +0800

    Exit gracefully when exception occurs in QP

--- Team32/Code32/src/spa/src/QP/QueryProcessor.cpp
+++ Team32/Code32/src/spa/src/QP/QueryProcessor.cpp
@@ -1,12 +1,22 @@
 #include "QP/QueryProcessor.h"
 
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+#include <utility>
+
 #include "QP/QueryPreprocessor.h"
 
 QueryProcessor::QueryProcessor(PKB& pkb) : evaluator(QueryEvaluator(pkb)) {}
 
 vector<string> QueryProcessor::processQuery(string query) {
-	QueryPreprocessor preprocessor;
-	QueryProperties query_properties = preprocessor.parseQuery(std::move(query));
-	QueryResult result = evaluator.executeQuery(query_properties);
-	return QueryFormatter::formatResult(query_properties, result);
+	try {
+		QueryPreprocessor preprocessor;
+		QueryProperties query_properties = preprocessor.parseQuery(std::move(query));
+		QueryResult result = evaluator.executeQuery(query_properties);
+		return QueryFormatter::formatResult(query_properties, result);
+	} catch (const exception& e) {
+		cout << e.what() << endl;
+		exit(1);
+	}
 }
