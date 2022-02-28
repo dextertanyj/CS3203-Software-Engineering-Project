c8d9a81d6e63e1a8436ca02c7c2dd87e05294040
Dexter Tan <dev@dextertanyj.com>
2022-02-28 20:42:32 +0800

    Implement procedure store

--- Team32/Code32/src/spa/src/PKB/ProcedureStore.h
+++ Team32/Code32/src/spa/src/PKB/ProcedureStore.h
@@ -4,26 +4,25 @@
 #include <memory>
 #include <unordered_map>
 #include <unordered_set>
+#include <vector>
 
 #include "Common/TypeDefs.h"
 #include "PKB/PKB.h"
+#include "PKB/ProcedureInfo.h"
 
 using namespace std;
 
 class PKB::ProcedureStore {
 public:
 	ProcedureStore();
-	void setProc(const ProcRef&, const vector<shared_ptr<StmtInfo>>&);
-	void setCall(const shared_ptr<StmtInfo>&, const ProcRef&);
-	vector<shared_ptr<StmtInfo>> getStmtsByProc(const ProcRef&);
-	ProcRef getProcByStmt(const shared_ptr<StmtInfo>&);
-	ProcRef getProcByCall(const shared_ptr<StmtInfo>&);
-	unordered_set<ProcRef> getProcListByStmtList(const unordered_set<shared_ptr<StmtInfo>>&);
+	void insert(const ProcRef& procedure, vector<shared_ptr<StmtInfo>> statements);
+	[[nodiscard]] shared_ptr<ProcedureInfo> get(const ProcRef& procedure) const;
+	[[nodiscard]] unordered_set<ProcRef> getAll() const;
+	[[nodiscard]] unordered_set<shared_ptr<ProcedureInfo>> getAllInfo() const;
 	void clear();
 
 private:
-	unordered_map<ProcRef, ProcRelation> proc_map;
-	unordered_map<StmtRef, ProcRef> stmt_to_proc_map;
+	unordered_map<ProcRef, shared_ptr<ProcedureInfo>> store;
 };
 
 #endif
