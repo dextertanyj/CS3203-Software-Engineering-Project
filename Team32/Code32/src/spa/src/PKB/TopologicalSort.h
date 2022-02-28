90cf65bc6fd457b46b24d874fd7ad797600d1205
Dexter Tan <dev@dextertanyj.com>
2022-02-28 20:43:32 +0800

    Implement topological sort

---
+++ Team32/Code32/src/spa/src/PKB/TopologicalSort.h
@@ -0,0 +1,24 @@
+#ifndef SPA_SRC_PKB_TOPOLOGICALSORT_H
+#define SPA_SRC_PKB_TOPOLOGICALSORT_H
+
+#include <memory>
+#include <vector>
+
+#include "PKB/PKB.h"
+#include "PKB/TransitiveRelationStore.tpp"
+
+using namespace std;
+
+template <class TInfo>
+class PKB::TopologicalSort {
+public:
+	TopologicalSort();
+	template <class TStore, typename TIdent, class TRelation>
+	void sort(const TStore& truth_store, const TransitiveRelationStore<TIdent, TInfo, TRelation>& transitive_store);
+	[[nodiscard]] vector<shared_ptr<TInfo>> get() const;
+
+private:
+	vector<shared_ptr<TInfo>> order;
+};
+
+#endif  // SPA_SRC_PKB_TOPOLOGICALSORT_H
