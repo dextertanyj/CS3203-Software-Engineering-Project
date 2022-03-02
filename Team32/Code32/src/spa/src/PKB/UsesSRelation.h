2e2a24585eee34acf431dc4ee1f3a492d3b0dfb4
Wei Kit <weikit89@gmail.com>
2022-03-02 14:38:42 +0800

    Refactor & Enhance optimize for UsesRelation

--- Team32/Code32/src/spa/src/PKB/UsesSRelation.h
+++ Team32/Code32/src/spa/src/PKB/UsesSRelation.h
@@ -1,16 +1,24 @@
 #ifndef SPA_SRC_PKB_USESRELATION_H
 #define SPA_SRC_PKB_USESRELATION_H
 
+#include "PKB/CallStatementStore.h"
+#include "PKB/PKB.h"
 #include "PKB/SVRelationStore.h"
 #include "PKB/StatementRelationStore.tpp"
 #include "PKB/Types.h"
+#include "PKB/TopologicalSort.h"
 
 class PKB::UsesSRelation {
 public:
+public:
 	static bool validate(SVRelationStore<UsesSRelation>* store, const shared_ptr<StmtInfo>& statement, const VarRef& variable);
 	static bool validate(SVRelationStore<UsesSRelation>* store, const shared_ptr<StmtInfo>& statement, const VarRefSet& variable);
-	static void optimize(Types::StatementStore& statement_store, StatementRelationStore<ParentRelation>& parent_store,
-	                     SVRelationStore<UsesSRelation>& store);
+	static void optimize(StatementRelationStore<ParentRelation>& parent_store, CallStatementStore call_store, Types::ProcedureStore proc_store,
+	                     TopologicalSort<ProcedureInfo> topo_order, SVRelationStore<UsesSRelation>& store);
+	static VarRefSet optimizeConditional(shared_ptr<StmtInfo> statement, StatementRelationStore<ParentRelation>& parent_store,
+	                                     SVRelationStore<UsesSRelation>& store);
+	static VarRefSet optimizeCall(shared_ptr<StmtInfo> statement, CallStatementStore call_store, Types::ProcedureStore proc_store,
+	                              SVRelationStore<UsesSRelation>& store);
 };
 
 #endif  // SPA_SRC_PKB_USESRELATION_H
