c8e9b52cc1f2c6d897ec411cb5f6a25dd3848b77
Wei Kit <weikit89@gmail.com>
2022-03-03 19:39:38 +0800

    Enhance quality of Optimize Unit test to include Call* relations

--- Team32/Code32/src/unit_testing/src/PKB/ModifiesSRelationTest.cpp
+++ Team32/Code32/src/unit_testing/src/PKB/ModifiesSRelationTest.cpp
@@ -4,17 +4,6 @@
 #include "catch.hpp"
 
 TEST_CASE("PKB::ModifiesSRelation Test") {
-	/* Nested conditional statements:
-	 * main_proc:
-	 * 1 --
-	 * 	\   \
-	 * 	 2   5
-	 * 	  \
-	 * 	   3 --- 4
-	 *
-	 * sub_proc (Called by s4):
-	 * 6
-	 * */
 	PKB::SVRelationStore<PKB::ModifiesSRelation> store = PKB::SVRelationStore<PKB::ModifiesSRelation>();
 	shared_ptr<StmtInfo> s1 = TestUtilities::createStmtInfo(1, StmtType::WhileStmt);
 	shared_ptr<StmtInfo> s2 = TestUtilities::createStmtInfo(2, StmtType::IfStmt);
@@ -22,20 +11,9 @@ TEST_CASE("PKB::ModifiesSRelation Test") {
 	shared_ptr<StmtInfo> s4 = TestUtilities::createStmtInfo(4, StmtType::Call);
 	shared_ptr<StmtInfo> s5 = TestUtilities::createStmtInfo(5, StmtType::Read);
 	shared_ptr<StmtInfo> s6 = TestUtilities::createStmtInfo(6, StmtType::Read);
-	shared_ptr<StmtInfo> s7 = TestUtilities::createStmtInfo(6, StmtType::Print);
-
-	PKB::Types::ProcedureStore procedure_store = PKB::Types::ProcedureStore();
-	PKB::CallStatementStore call_statement_store = PKB::CallStatementStore();
-	PKB::TopologicalSort<PKB::ProcedureInfo> call_graph = PKB::TopologicalSort<PKB::ProcedureInfo>();
-	PKB::TransitiveRelationStore<ProcRef, PKB::ProcedureInfo, PKB::CallRelation> transitive_rel_store =
-		PKB::TransitiveRelationStore<ProcRef, PKB::ProcedureInfo, PKB::CallRelation>();
-	ProcRef main_proc = "main";
-	ProcRef sub_proc = "sub";
-	call_statement_store.set(s4, sub_proc);
-	procedure_store.insert(main_proc, {s1, s2, s3, s4, s5});
-	procedure_store.insert(sub_proc, {s6});
-	transitive_rel_store.set(procedure_store.get(main_proc), procedure_store.get(sub_proc));
-	call_graph.sort(procedure_store, transitive_rel_store);
+	shared_ptr<StmtInfo> s7 = TestUtilities::createStmtInfo(7, StmtType::Call);
+	shared_ptr<StmtInfo> s8 = TestUtilities::createStmtInfo(8, StmtType::Assign);
+	shared_ptr<StmtInfo> s9 = TestUtilities::createStmtInfo(9, StmtType::Print);
 
 	SECTION("PKB::ModifiesSRelation::validate One Var Test") {
 		REQUIRE(PKB::ModifiesSRelation::validate(&store, s1, "x"));
@@ -46,7 +24,7 @@ TEST_CASE("PKB::ModifiesSRelation Test") {
 		REQUIRE(PKB::ModifiesSRelation::validate(&store, s6, "x"));
 
 		// Print statements cannot modify a variable.
-		REQUIRE_THROWS_AS(PKB::ModifiesSRelation::validate(&store, s7, "x"), invalid_argument);
+		REQUIRE_THROWS_AS(PKB::ModifiesSRelation::validate(&store, s9, "x"), invalid_argument);
 
 		store.set(s1, "x");
 		store.set(s2, "x");
@@ -68,15 +46,49 @@ TEST_CASE("PKB::ModifiesSRelation Test") {
 		REQUIRE(PKB::ModifiesSRelation::validate(&store, s4, var_refs));
 		REQUIRE_FALSE(PKB::ModifiesSRelation::validate(&store, s5, var_refs));
 		REQUIRE_FALSE(PKB::ModifiesSRelation::validate(&store, s6, var_refs));
-		REQUIRE_THROWS_AS(PKB::ModifiesSRelation::validate(&store, s7, var_refs), invalid_argument);
+		REQUIRE_THROWS_AS(PKB::ModifiesSRelation::validate(&store, s9, var_refs), invalid_argument);
 	}
-	
+
 	SECTION("PKB::ModifiesSRelation::optimize Test") {
+		/* Nested conditional statements:
+		 * main_proc:
+		 * s1 ---
+		 * 	 \   \
+		 * 	  s2  s5
+		 * 	   \
+		 * 	    s3 -- s4
+		 *
+		 * sub_proc_1 (Called by s4):
+		 * s6 -- s7
+		 *
+		 * sub_proc_2 (called by s7):
+		 * s8
+		 */
+		// Initialise necessary stores for optimize.
+		PKB::Types::ProcedureStore procedure_store = PKB::Types::ProcedureStore();
+		PKB::CallStatementStore call_statement_store = PKB::CallStatementStore();
+		PKB::TopologicalSort<PKB::ProcedureInfo> call_graph = PKB::TopologicalSort<PKB::ProcedureInfo>();
+		PKB::TransitiveRelationStore<ProcRef, PKB::ProcedureInfo, PKB::CallRelation> transitive_rel_store =
+			PKB::TransitiveRelationStore<ProcRef, PKB::ProcedureInfo, PKB::CallRelation>();
+
+		ProcRef main_proc = "main";
+		ProcRef sub_proc_1 = "sub_1";
+		ProcRef sub_proc_2 = "sub_2";
+		call_statement_store.set(s4, sub_proc_1);
+		call_statement_store.set(s7, sub_proc_2);
+		procedure_store.insert(main_proc, {s1, s2, s3, s4, s5});
+		procedure_store.insert(sub_proc_1, {s6, s7});
+		procedure_store.insert(sub_proc_2, {s8});
+		transitive_rel_store.set(procedure_store.get(main_proc), procedure_store.get(sub_proc_1));
+		transitive_rel_store.set(procedure_store.get(sub_proc_1), procedure_store.get(sub_proc_2));
+		call_graph.sort(procedure_store, transitive_rel_store);
+
 		PKB::StatementRelationStore<PKB::ParentRelation> parent_store = PKB::StatementRelationStore<PKB::ParentRelation>();
 
 		store.set(s3, "z");
 		store.set(s5, "a");
 		store.set(s6, "b");
+		store.set(s8, "c");
 		parent_store.set(s1, s2);
 		parent_store.set(s1, s5);
 		parent_store.set(s2, s4);
@@ -85,21 +97,25 @@ TEST_CASE("PKB::ModifiesSRelation Test") {
 		PKB::ParentRelation::optimize(parent_store);
 		PKB::ModifiesSRelation::optimize(parent_store, call_statement_store, procedure_store, call_graph, store);
 
-		// If optimize works correctly, var "b" from s6 in sub_proc should
-		// propagate to s4 in main_proc, which would propagate up to s2 and s1.
-		// Var "a" from s5 should propagate up to s1, and var "z" from s3 should
-		// propagate up to s2 and s1.
-
-		VarRefSet expected_set_s1 = {"z", "a", "b"};
-		VarRefSet expected_set_s2 = {"z", "b"};
+		/* If optimize works correctly:
+		 * 1) var 'c' from s8 in sub_proc_2 propagates up to s7, s4, s2, and s1.
+		 * 2) var 'b' from s6 in sub_proc_1 propagates to s4, s2 and s1.
+		 * 3) var 'a' from s5 propagates up to s1,
+		 * 4) var 'z' from s3 propagates up to s2 and s1.
+		*/
+		VarRefSet expected_set_s1 = {"z", "a", "b", "c"};
+		VarRefSet expected_set_s2 = {"z", "b", "c"};
 		VarRefSet expected_set_s3 = {"z"};
-		VarRefSet expected_set_s4 = {"b"};
+		VarRefSet expected_set_s4 = {"b", "c"};
 		VarRefSet expected_set_s5 = {"a"};
 		VarRefSet expected_set_s6 = {"b"};
+		VarRefSet expected_set_s7 = {"c"};
+		VarRefSet expected_set_s8 = {"c"};
 
 		StmtInfoPtrSet expected_set_var_z = {s1, s2, s3};
 		StmtInfoPtrSet expected_set_var_a = {s1, s5};
 		StmtInfoPtrSet expected_set_var_b = {s1, s2, s4, s6};
+		StmtInfoPtrSet expected_set_var_c = {s1, s2, s4, s6};
 
 		CHECK(store.getByStmt(1) == expected_set_s1);
 		CHECK(store.getByStmt(2) == expected_set_s2);
@@ -107,9 +123,12 @@ TEST_CASE("PKB::ModifiesSRelation Test") {
 		CHECK(store.getByStmt(4) == expected_set_s4);
 		CHECK(store.getByStmt(5) == expected_set_s5);
 		CHECK(store.getByStmt(6) == expected_set_s6);
+		CHECK(store.getByStmt(7) == expected_set_s7);
+		CHECK(store.getByStmt(8) == expected_set_s8);
 
 		CHECK(store.getByVar("z") == expected_set_var_z);
 		CHECK(store.getByVar("a") == expected_set_var_a);
 		CHECK(store.getByVar("b") == expected_set_var_b);
+		CHECK(store.getByVar("c") == expected_set_var_c);
 	}
 }
