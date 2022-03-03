58f63ac023b7de9fb60fe61b41e197aacaf1b736
Tan Yan Lyn <e0424594@u.nus.edu>
2022-03-03 10:39:18 +0800

    Fix lint

--- Team32/Code32/src/spa/src/PKB/ModifiesPRelation.cpp
+++ Team32/Code32/src/spa/src/PKB/ModifiesPRelation.cpp
@@ -1,8 +1,8 @@
 #include "PKB/ModifiesPRelation.h"
 
-void PKB::ModifiesPRelation::optimize(const Types::ProcedureStore &procStore, PVRelationStore<ModifiesPRelation> &store,
+void PKB::ModifiesPRelation::optimize(const Types::ProcedureStore &proc_store, PVRelationStore<ModifiesPRelation> &store,
                                       SVRelationStore<ModifiesSRelation> &modifies_s_store) {
-	for (const auto &proc_info : procStore.getAll()) {
+	for (const auto &proc_info : proc_store.getAll()) {
 		vector<shared_ptr<StmtInfo>> proc_stmt_list = proc_info->getStatements();
 		VarRefSet variables;
 		for (const auto &statement : proc_stmt_list) {
--- Team32/Code32/src/spa/src/PKB/UsesPRelation.cpp
+++ Team32/Code32/src/spa/src/PKB/UsesPRelation.cpp
@@ -1,8 +1,8 @@
 #include "PKB/UsesPRelation.h"
 
-void PKB::UsesPRelation::optimize(const Types::ProcedureStore& procStore, PVRelationStore<UsesPRelation>& store,
+void PKB::UsesPRelation::optimize(const Types::ProcedureStore& proc_store, PVRelationStore<UsesPRelation>& store,
                                   SVRelationStore<UsesSRelation>& uses_s_store) {
-	for (const auto& proc_info : procStore.getAll()) {
+	for (const auto& proc_info : proc_store.getAll()) {
 		vector<shared_ptr<StmtInfo>> proc_stmt_list = proc_info->getStatements();
 		VarRefSet variables;
 		for (const auto& statement : proc_stmt_list) {
