9e3fbb93576b46c9bfe312ac00c51bf212785ccc
Dexter Tan <dev@dextertanyj.com>
2022-03-08 10:58:51 +0800

    Update autotester libraries

--- Team32/Code32/src/autotester/src/TestWrapper.h
+++ Team32/Code32/src/autotester/src/TestWrapper.h
@@ -1,33 +1,34 @@
 #ifndef TESTWRAPPER_H
 #define TESTWRAPPER_H
 
-#include <string>
 #include <iostream>
 #include <list>
+#include <string>
 
 // include your other headers here
 #include "AbstractWrapper.h"
 #include "PKB/Storage.h"
-#include "SP/Processor.h"
 #include "QP/QueryProcessor.h"
+#include "SP/Processor.h"
 
 class TestWrapper : public AbstractWrapper {
- public:
-  // default constructor
-  TestWrapper();
-  
-  // destructor
-  ~TestWrapper();
-  
-  // method for parsing the SIMPLE source
-  virtual void parse(std::string filename);
-  
-  // method for evaluating a query
-  virtual void evaluate(std::string query, std::list<std::string>& results);
-  private:
-	  PKB::Storage pkb;
-	  SP::Processor source_processor;
-	  QP::QueryProcessor query_processor;
+public:
+	// default constructor
+	TestWrapper();
+
+	// destructor
+	~TestWrapper();
+
+	// method for parsing the SIMPLE source
+	virtual void parse(std::string filename);
+
+	// method for evaluating a query
+	virtual void evaluate(std::string query, std::list<std::string>& results);
+
+private:
+	PKB::Storage pkb;
+	SP::Processor source_processor;
+	QP::QueryProcessor query_processor;
 };
 
 #endif
