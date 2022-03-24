52f704b14da24d77fbf4071d35d88ed8a93dacf0
samlsm <samlsm2803@gmail.com>
2022-03-25 00:29:44 +0800

    Minor Fix

--- Team32/Code32/src/unit_testing/src/QP/Executor/WithExecutorTest.cpp
+++ Team32/Code32/src/unit_testing/src/QP/Executor/WithExecutorTest.cpp
@@ -76,161 +76,164 @@ TEST_CASE("WithExecutor::execute") {
 
 	SECTION("Trivial: Attribute & Attribute") {
 		QP::QueryResult result1 = WithExecutor::executeTrivialAttributeAttribute(store, proc, call, proc_internal_executor, call_internal_executor);
-		QP::QueryResult result2 = WithExecutor::executeTrivialAttributeAttribute(store, read, print, read_internal_executor, print_internal_executor);
-		QP::QueryResult result3 = WithExecutor::executeTrivialAttributeAttribute(store, stmt, constant, stmt_internal_executor, constant_internal_executor);
-		QP::QueryResult result4 = WithExecutor::executeTrivialAttributeAttribute(store, proc, proc, proc_internal_executor, proc_internal_executor);
-		QP::QueryResult result5 = WithExecutor::executeTrivialAttributeAttribute(store, proc, read, proc_internal_executor, read_internal_executor);
-		QP::QueryResult result6 = WithExecutor::executeTrivialAttributeAttribute(store, if_stmt, constant, if_stmt_internal_executor, constant_internal_executor);
 		REQUIRE(result1.getResult());
+		QP::QueryResult result2 = WithExecutor::executeTrivialAttributeAttribute(store, read, print, read_internal_executor, print_internal_executor);
 		REQUIRE(result2.getResult());
+		QP::QueryResult result3 = WithExecutor::executeTrivialAttributeAttribute(store, stmt, constant, stmt_internal_executor, constant_internal_executor);
 		REQUIRE(result3.getResult());
+		QP::QueryResult result4 = WithExecutor::executeTrivialAttributeAttribute(store, proc, proc, proc_internal_executor, proc_internal_executor);
 		REQUIRE(result4.getResult());
+		QP::QueryResult result5 = WithExecutor::executeTrivialAttributeAttribute(store, proc, read, proc_internal_executor, read_internal_executor);
 		REQUIRE_FALSE(result5.getResult());
+		QP::QueryResult result6 = WithExecutor::executeTrivialAttributeAttribute(store, if_stmt, constant, if_stmt_internal_executor, constant_internal_executor);
 		REQUIRE_FALSE(result6.getResult());
 	}
 
 	SECTION("Trivial: Attribute & Constant") {
 		QP::QueryResult result1 = WithExecutor::executeTrivialAttributeConstant(store, proc, main, proc_internal_executor, name_internal_executor);
-		QP::QueryResult result2 = WithExecutor::executeTrivialAttributeConstant(store, read, x, read_internal_executor, name_internal_executor);
-		QP::QueryResult result3 = WithExecutor::executeTrivialAttributeConstant(store, if_stmt, index_5, if_stmt_internal_executor, index_internal_executor);
-		QP::QueryResult result4 = WithExecutor::executeTrivialAttributeConstant(store, read, y, read_internal_executor, name_internal_executor);
-		QP::QueryResult result5 = WithExecutor::executeTrivialAttributeConstant(store, call, main, call_internal_executor, name_internal_executor);
-		QP::QueryResult result6 = WithExecutor::executeTrivialAttributeConstant(store, if_stmt, invalid_index, if_stmt_internal_executor, index_internal_executor);
-		QP::QueryResult result7 = WithExecutor::executeTrivialAttributeConstant(store, stmt, invalid_index, if_stmt_internal_executor, index_internal_executor);
 		REQUIRE(result1.getResult());
+		QP::QueryResult result2 = WithExecutor::executeTrivialAttributeConstant(store, read, x, read_internal_executor, name_internal_executor);
 		REQUIRE(result2.getResult());
+		QP::QueryResult result3 = WithExecutor::executeTrivialAttributeConstant(store, if_stmt, index_5, if_stmt_internal_executor, index_internal_executor);
 		REQUIRE(result3.getResult());
+		QP::QueryResult result4 = WithExecutor::executeTrivialAttributeConstant(store, read, y, read_internal_executor, name_internal_executor);
 		REQUIRE_FALSE(result4.getResult());
+		QP::QueryResult result5 = WithExecutor::executeTrivialAttributeConstant(store, call, main, call_internal_executor, name_internal_executor);
 		REQUIRE_FALSE(result5.getResult());
+		QP::QueryResult result6 = WithExecutor::executeTrivialAttributeConstant(store, if_stmt, invalid_index, if_stmt_internal_executor, index_internal_executor);
 		REQUIRE_FALSE(result6.getResult());
+		QP::QueryResult result7 = WithExecutor::executeTrivialAttributeConstant(store, stmt, invalid_index, if_stmt_internal_executor, index_internal_executor);
 		REQUIRE_FALSE(result7.getResult());
 	}
 
 
 	SECTION("Trivial: Constant & Attribute") {
 		QP::QueryResult result1 = WithExecutor::executeTrivialConstantAttribute(store, main, proc, name_internal_executor, proc_internal_executor);
-		QP::QueryResult result2 = WithExecutor::executeTrivialConstantAttribute(store, x, read, name_internal_executor, read_internal_executor);
-		QP::QueryResult result3 = WithExecutor::executeTrivialConstantAttribute(store, index_5, if_stmt, index_internal_executor, if_stmt_internal_executor);
-		QP::QueryResult result4 = WithExecutor::executeTrivialConstantAttribute(store, y, read, name_internal_executor, read_internal_executor);
-		QP::QueryResult result5 = WithExecutor::executeTrivialConstantAttribute(store, main, call, name_internal_executor, call_internal_executor);
-		QP::QueryResult result6 = WithExecutor::executeTrivialConstantAttribute(store, invalid_index, if_stmt, index_internal_executor, if_stmt_internal_executor);
-		QP::QueryResult result7 = WithExecutor::executeTrivialConstantAttribute(store, invalid_index, stmt, index_internal_executor, if_stmt_internal_executor);
 		REQUIRE(result1.getResult());
+		QP::QueryResult result2 = WithExecutor::executeTrivialConstantAttribute(store, x, read, name_internal_executor, read_internal_executor);
 		REQUIRE(result2.getResult());
+		QP::QueryResult result3 = WithExecutor::executeTrivialConstantAttribute(store, index_5, if_stmt, index_internal_executor, if_stmt_internal_executor);
 		REQUIRE(result3.getResult());
+		QP::QueryResult result4 = WithExecutor::executeTrivialConstantAttribute(store, y, read, name_internal_executor, read_internal_executor);
 		REQUIRE_FALSE(result4.getResult());
+		QP::QueryResult result5 = WithExecutor::executeTrivialConstantAttribute(store, main, call, name_internal_executor, call_internal_executor);
 		REQUIRE_FALSE(result5.getResult());
+		QP::QueryResult result6 = WithExecutor::executeTrivialConstantAttribute(store, invalid_index, if_stmt, index_internal_executor, if_stmt_internal_executor);
 		REQUIRE_FALSE(result6.getResult());
+		QP::QueryResult result7 = WithExecutor::executeTrivialConstantAttribute(store, invalid_index, stmt, index_internal_executor, if_stmt_internal_executor);
 		REQUIRE_FALSE(result7.getResult());
 	}
 
 	SECTION("Trivial: Constant & Constant") {
 		QP::QueryResult result1 = WithExecutor::executeTrivialConstantConstant(store, main, main, name_internal_executor, name_internal_executor);
-		QP::QueryResult result2 = WithExecutor::executeTrivialConstantConstant(store, index_5, index_5, index_internal_executor, index_internal_executor);
-		QP::QueryResult result3 = WithExecutor::executeTrivialConstantConstant(store, main, mars, name_internal_executor, name_internal_executor);
-		QP::QueryResult result4 = WithExecutor::executeTrivialConstantConstant(store, index_5, invalid_index, index_internal_executor, index_internal_executor);
 		REQUIRE(result1.getResult());
+		QP::QueryResult result2 = WithExecutor::executeTrivialConstantConstant(store, index_5, index_5, index_internal_executor, index_internal_executor);
 		REQUIRE(result2.getResult());
+		QP::QueryResult result3 = WithExecutor::executeTrivialConstantConstant(store, main, mars, name_internal_executor, name_internal_executor);
 		REQUIRE_FALSE(result3.getResult());
+		QP::QueryResult result4 = WithExecutor::executeTrivialConstantConstant(store, index_5, invalid_index, index_internal_executor, index_internal_executor);
 		REQUIRE_FALSE(result4.getResult());
 	}
 
 	SECTION("Attribute & Attribute") {
 		QP::QueryResult result1 = WithExecutor::executeAttributeAttribute(store, proc, call, proc_internal_executor, call_internal_executor);
-		QP::QueryResult result2 = WithExecutor::executeAttributeAttribute(store, read, print, read_internal_executor, print_internal_executor);
-		QP::QueryResult result3 = WithExecutor::executeAttributeAttribute(store, stmt, constant, stmt_internal_executor, constant_internal_executor);
-		QP::QueryResult result4 = WithExecutor::executeAttributeAttribute(store, proc, proc, proc_internal_executor, proc_internal_executor);
-		QP::QueryResult result5 = WithExecutor::executeAttributeAttribute(store, variable, print, variable_internal_executor, print_internal_executor);
-		QP::QueryResult result6 = WithExecutor::executeAttributeAttribute(store, proc, read, proc_internal_executor, read_internal_executor);
-		QP::QueryResult result7 = WithExecutor::executeAttributeAttribute(store, if_stmt, constant, if_stmt_internal_executor, constant_internal_executor);
-		QP::QueryResult result8 = WithExecutor::executeAttributeAttribute(store, variable, call, variable_internal_executor, call_internal_executor);
-
 		vector<string> expected_result_1 = {"mars"};
+		REQUIRE(result1.getSynonymResult("p") == expected_result_1);
+
+		QP::QueryResult result2 = WithExecutor::executeAttributeAttribute(store, read, print, read_internal_executor, print_internal_executor);
 		vector<string> expected_result_2_r = {"4", "4"};
 		vector<string> expected_result_2_p = {"1", "7"};
-		vector<string> expected_result_3 = {"1", "3"};
-		vector<string> expected_result_4 = {"main", "mars"};
-		vector<string> expected_result_5 = {"x", "x"};
-
-		REQUIRE(result1.getSynonymResult("p") == expected_result_1);
 		REQUIRE(result2.getSynonymResult("r") == expected_result_2_r);
 		vector<string> actual_result_2_print = result2.getSynonymResult("pr");
 		sort(actual_result_2_print.begin(), actual_result_2_print.end());
 		REQUIRE(actual_result_2_print == expected_result_2_p);
+
+		QP::QueryResult result3 = WithExecutor::executeAttributeAttribute(store, stmt, constant, stmt_internal_executor, constant_internal_executor);
+		vector<string> expected_result_3 = {"1", "3"};
 		vector<string> actual_result_3_const = result3.getSynonymResult("const");
 		vector<string> actual_result_3_stmt = result3.getSynonymResult("s");
 		sort(actual_result_3_const.begin(), actual_result_3_const.end());
 		sort(actual_result_3_stmt.begin(), actual_result_3_stmt.end());
 		REQUIRE(actual_result_3_const == expected_result_3);
 		REQUIRE(actual_result_3_stmt == expected_result_3);
+
+		QP::QueryResult result4 = WithExecutor::executeAttributeAttribute(store, proc, proc, proc_internal_executor, proc_internal_executor);
+		vector<string> expected_result_4 = {"main", "mars"};
 		vector<string> actual_result_4 = result4.getSynonymResult("p");
 		sort(actual_result_4.begin(), actual_result_4.end());
 		REQUIRE(actual_result_4 == expected_result_4);
+
+		QP::QueryResult result5 = WithExecutor::executeAttributeAttribute(store, variable, print, variable_internal_executor, print_internal_executor);
+		vector<string> expected_result_5 = {"x", "x"};
 		vector<string> actual_result_5 = result5.getSynonymResult("v");
 		sort(actual_result_5.begin(), actual_result_5.end());
 		REQUIRE(actual_result_5 == expected_result_5);
+
+		QP::QueryResult result6 = WithExecutor::executeAttributeAttribute(store, proc, read, proc_internal_executor, read_internal_executor);
 		REQUIRE_FALSE(result6.getResult());
+		QP::QueryResult result7 = WithExecutor::executeAttributeAttribute(store, if_stmt, constant, if_stmt_internal_executor, constant_internal_executor);
 		REQUIRE_FALSE(result7.getResult());
+		QP::QueryResult result8 = WithExecutor::executeAttributeAttribute(store, variable, call, variable_internal_executor, call_internal_executor);
 		REQUIRE_FALSE(result8.getResult());
 	}
 
 	SECTION("Attribute and Constant") {
 		QP::QueryResult result1 = WithExecutor::executeAttributeConstant(store, proc, mars, proc_internal_executor, name_internal_executor);
-		QP::QueryResult result2 = WithExecutor::executeAttributeConstant(store, call, mars, call_internal_executor, name_internal_executor);
-		QP::QueryResult result3 = WithExecutor::executeAttributeConstant(store, while_stmt, index_3, while_stmt_internal_executor, index_internal_executor);
-		QP::QueryResult result4 = WithExecutor::executeAttributeConstant(store, variable, x, variable_internal_executor, name_internal_executor);
-		QP::QueryResult result5 = WithExecutor::executeAttributeConstant(store, print, x, print_internal_executor, name_internal_executor);
-		QP::QueryResult result6 = WithExecutor::executeAttributeConstant(store,  call, main, call_internal_executor, name_internal_executor);
-		QP::QueryResult result7 = WithExecutor::executeAttributeConstant(store, while_stmt, index_5, while_stmt_internal_executor, index_internal_executor);
-		QP::QueryResult result8 = WithExecutor::executeAttributeConstant(store, print, y, print_internal_executor, name_internal_executor);
-		QP::QueryResult result9 = WithExecutor::executeAttributeConstant(store, variable, invalid_name, variable_internal_executor, name_internal_executor);
-
 		vector<string> expected_result_1 = {"mars"};
-		vector<string> expected_result_2 = {"2"};
-		vector<string> expected_result_3 = {"3"};
-		vector<string> expected_result_4 = {"x"};
-		vector<string> expected_result_5 = {"1", "7"};
-
 		REQUIRE(result1.getSynonymResult("p") == expected_result_1);
+		QP::QueryResult result2 = WithExecutor::executeAttributeConstant(store, call, mars, call_internal_executor, name_internal_executor);
+		vector<string> expected_result_2 = {"2"};
 		REQUIRE(result2.getSynonymResult("c") == expected_result_2);
+		QP::QueryResult result3 = WithExecutor::executeAttributeConstant(store, while_stmt, index_3, while_stmt_internal_executor, index_internal_executor);
+		vector<string> expected_result_3 = {"3"};
 		REQUIRE(result3.getSynonymResult("while") == expected_result_3);
+		QP::QueryResult result4 = WithExecutor::executeAttributeConstant(store, variable, x, variable_internal_executor, name_internal_executor);
+		vector<string> expected_result_4 = {"x"};
 		REQUIRE(result4.getSynonymResult("v") == expected_result_4);
+
+		QP::QueryResult result5 = WithExecutor::executeAttributeConstant(store, print, x, print_internal_executor, name_internal_executor);
+		vector<string> expected_result_5 = {"1", "7"};
 		vector<string> actual_result_5 = result5.getSynonymResult("pr");
 		sort(actual_result_5.begin(), actual_result_5.end());
 		REQUIRE(actual_result_5 == expected_result_5);
+
+		QP::QueryResult result6 = WithExecutor::executeAttributeConstant(store,  call, main, call_internal_executor, name_internal_executor);
 		REQUIRE_FALSE(result6.getResult());
+		QP::QueryResult result7 = WithExecutor::executeAttributeConstant(store, while_stmt, index_5, while_stmt_internal_executor, index_internal_executor);
 		REQUIRE_FALSE(result7.getResult());
+		QP::QueryResult result8 = WithExecutor::executeAttributeConstant(store, print, y, print_internal_executor, name_internal_executor);
 		REQUIRE_FALSE(result8.getResult());
+		QP::QueryResult result9 = WithExecutor::executeAttributeConstant(store, variable, invalid_name, variable_internal_executor, name_internal_executor);
 		REQUIRE_FALSE(result9.getResult());
 	}
 
 	SECTION("Constant and Attribute") {
 		QP::QueryResult result1 = WithExecutor::executeConstantAttribute(store, mars, proc, name_internal_executor, proc_internal_executor);
-		QP::QueryResult result2 = WithExecutor::executeConstantAttribute(store, mars, call, name_internal_executor, call_internal_executor);
-		QP::QueryResult result3 = WithExecutor::executeConstantAttribute(store, index_6, assign_stmt, index_internal_executor, assign_stmt_internal_executor);
-		QP::QueryResult result4 = WithExecutor::executeConstantAttribute(store, x, variable, name_internal_executor, variable_internal_executor);
-		QP::QueryResult result5 = WithExecutor::executeConstantAttribute(store, x, print, name_internal_executor, print_internal_executor);
-		QP::QueryResult result6 = WithExecutor::executeConstantAttribute(store,  main, call, name_internal_executor, call_internal_executor);
-		QP::QueryResult result7 = WithExecutor::executeConstantAttribute(store, index_5, while_stmt, index_internal_executor, while_stmt_internal_executor);
-		QP::QueryResult result8 = WithExecutor::executeConstantAttribute(store, y, print, name_internal_executor, print_internal_executor);
-		QP::QueryResult result9 = WithExecutor::executeConstantAttribute(store, invalid_name, variable, name_internal_executor, variable_internal_executor);
-
 		vector<string> expected_result_1 = {"mars"};
-		vector<string> expected_result_2 = {"2"};
-		vector<string> expected_result_3 = {"6"};
-		vector<string> expected_result_4 = {"x"};
-		vector<string> expected_result_5 = {"1", "7"};
-
 		REQUIRE(result1.getSynonymResult("p") == expected_result_1);
+		QP::QueryResult result2 = WithExecutor::executeConstantAttribute(store, mars, call, name_internal_executor, call_internal_executor);
+		vector<string> expected_result_2 = {"2"};
 		REQUIRE(result2.getSynonymResult("c") == expected_result_2);
+		QP::QueryResult result3 = WithExecutor::executeConstantAttribute(store, index_6, assign_stmt, index_internal_executor, assign_stmt_internal_executor);
+		vector<string> expected_result_3 = {"6"};
 		REQUIRE(result3.getSynonymResult("a") == expected_result_3);
+		QP::QueryResult result4 = WithExecutor::executeConstantAttribute(store, x, variable, name_internal_executor, variable_internal_executor);
+		vector<string> expected_result_4 = {"x"};
 		REQUIRE(result4.getSynonymResult("v") == expected_result_4);
+
+		QP::QueryResult result5 = WithExecutor::executeConstantAttribute(store, x, print, name_internal_executor, print_internal_executor);
+		vector<string> expected_result_5 = {"1", "7"};
 		vector<string> actual_result_5 = result5.getSynonymResult("pr");
 		sort(actual_result_5.begin(), actual_result_5.end());
 		REQUIRE(actual_result_5 == expected_result_5);
+
+		QP::QueryResult result6 = WithExecutor::executeConstantAttribute(store,  main, call, name_internal_executor, call_internal_executor);
 		REQUIRE_FALSE(result6.getResult());
+		QP::QueryResult result7 = WithExecutor::executeConstantAttribute(store, index_5, while_stmt, index_internal_executor, while_stmt_internal_executor);
 		REQUIRE_FALSE(result7.getResult());
+		QP::QueryResult result8 = WithExecutor::executeConstantAttribute(store, y, print, name_internal_executor, print_internal_executor);
 		REQUIRE_FALSE(result8.getResult());
+		QP::QueryResult result9 = WithExecutor::executeConstantAttribute(store, invalid_name, variable, name_internal_executor, variable_internal_executor);
 		REQUIRE_FALSE(result9.getResult());
 	}
 }
