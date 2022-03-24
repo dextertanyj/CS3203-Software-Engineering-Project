70fdce1e57c461519b7716da393180123e0059d6
Wei Kit <weikit89@gmail.com>
2022-03-24 22:49:26 +0800

    Add getProcedure method which was mistakenly deleted during rebase

--- Team32/Code32/src/spa/src/PKB/CallsStatementStore.cpp
+++ Team32/Code32/src/spa/src/PKB/CallsStatementStore.cpp
@@ -4,42 +4,50 @@
 
 PKB::CallsStatementStore::CallsStatementStore() = default;
 
-void PKB::CallsStatementStore::set(const shared_ptr<StmtInfo>& statement, ProcRef procedure) {
-	if (statement->getType() != StmtType::Call) {
-		throw logic_error("Invalid statement type for call store.");
-	}
-	StmtRef index = statement->getIdentifier();
-	if (map.find(index) != map.end()) {
-		throw logic_error("Call statement already set.");
-	}
-	map.insert({index, std::move(procedure)});
+void PKB::CallsStatementStore::set(const shared_ptr<StmtInfo> &statement, ProcRef procedure) {
+    if (statement->getType() != StmtType::Call) {
+        throw logic_error("Invalid statement type for call store.");
+    }
+    StmtRef index = statement->getIdentifier();
+    if (map.find(index) != map.end()) {
+        throw logic_error("Call statement already set.");
+    }
+    map.insert({index, std::move(procedure)});
 }
 
-ProcRef PKB::CallsStatementStore::getProcedure(const shared_ptr<StmtInfo>& statement) const {
-	if (statement->getType() != StmtType::Call) {
-		throw logic_error("Invalid statement type for call store.");
-	}
-	StmtRef index = statement->getIdentifier();
-	auto iter = map.find(index);
-	if (iter == map.end()) {
-		throw logic_error("Call statement not set.");
-	}
-	return iter->second;
+ProcRef PKB::CallsStatementStore::getProcedure(const shared_ptr<StmtInfo> &statement) const {
+    if (statement->getType() != StmtType::Call) {
+        throw logic_error("Invalid statement type for call store.");
+    }
+    StmtRef index = statement->getIdentifier();
+    auto iter = map.find(index);
+    if (iter == map.end()) {
+        throw logic_error("Call statement not set.");
+    }
+    return iter->second;
 }
 
-void PKB::CallsStatementStore::populate(const PKB::Types::ProcedureStore& procedures,
-                                        PKB::TransitiveRelationStore<ProcRef, PKB::ProcedureInfo, PKB::CallsRelation>& call_store) const {
-	for (const shared_ptr<ProcedureInfo>& procedure : procedures.getAll()) {
-		for (const shared_ptr<StmtInfo>& statement : procedure->getStatements()) {
-			if (statement->getType() != StmtType::Call) {
-				continue;
-			}
-			ProcRef callee_name = getProcedure(statement);
-			shared_ptr<ProcedureInfo> callee = procedures.get(callee_name);
-			if (callee == nullptr) {
-				throw logic_error("Procedure does not exist.");
-			}
-			call_store.set(procedure, callee);
-		}
-	}
+ProcRef PKB::CallsStatementStore::getProcedure(StmtRef index) const {
+    auto iter = map.find(index);
+    if (iter == map.end()) {
+        throw logic_error("Statement not found.");
+    }
+    return iter->second;
+}
+
+void PKB::CallsStatementStore::populate(const PKB::Types::ProcedureStore &procedures,
+                                        PKB::TransitiveRelationStore<ProcRef, PKB::ProcedureInfo, PKB::CallsRelation> &call_store) const {
+    for (const shared_ptr<ProcedureInfo> &procedure: procedures.getAll()) {
+        for (const shared_ptr<StmtInfo> &statement: procedure->getStatements()) {
+            if (statement->getType() != StmtType::Call) {
+                continue;
+            }
+            ProcRef callee_name = getProcedure(statement);
+            shared_ptr<ProcedureInfo> callee = procedures.get(callee_name);
+            if (callee == nullptr) {
+                throw logic_error("Procedure does not exist.");
+            }
+            call_store.set(procedure, callee);
+        }
+    }
 }
