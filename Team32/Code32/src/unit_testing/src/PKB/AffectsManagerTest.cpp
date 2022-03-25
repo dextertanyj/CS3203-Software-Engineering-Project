2e3be3ae6d5003f4409081ab5584a95dc614e128
Wei Kit <weikit89@gmail.com>
2022-03-25 16:46:30 +0800

    Add checkAffects Unit Test

--- Team32/Code32/src/unit_testing/src/PKB/AffectsManagerTest.cpp
+++ Team32/Code32/src/unit_testing/src/PKB/AffectsManagerTest.cpp
@@ -432,3 +432,100 @@ TEST_CASE("PKB::AffectsManager::getAffected Test") {
                 StmtInfoPtrSet{assign_stmt_1, assign_stmt_3, assign_stmt_6, assign_stmt_9});
     }
 }
+
+
+TEST_CASE("PKB::AffectsManager::checkAffects Test") {
+    PKB::ControlFlowGraph cfg = PKB::ControlFlowGraph();
+    PKB::NextManager next_manager = PKB::NextManager(cfg);
+    PKB::SVRelationStore<PKB::ModifiesSRelation> modifies_store;
+    PKB::SVRelationStore<PKB::UsesSRelation> uses_store;
+    PKB::AffectsManager affects_manager = PKB::AffectsManager(cfg, modifies_store, uses_store);
+    /* SIMPLE Code:
+     * 1. x = 5;
+     * 2. while (a > 0) {
+     * 3.   x = x + 1;
+     *    }
+     * 4. while (y < 2);
+     * 5.   b = x - 1;
+     * 6.   x = b + 1;
+     * 7.   while (a == 1) {
+     * 8.     read x;
+     * 9.     x = x + 1;
+     *      }
+     *    }
+     * 10.z = x;
+     */
+    shared_ptr<StmtInfo> assign_stmt_1 = TestUtilities::createStmtInfo(1, StmtType::Assign);
+    shared_ptr<StmtInfo> while_stmt_2 = TestUtilities::createStmtInfo(2, StmtType::WhileStmt);
+    shared_ptr<StmtInfo> assign_stmt_3 = TestUtilities::createStmtInfo(3, StmtType::Assign);
+    shared_ptr<StmtInfo> while_stmt_4 = TestUtilities::createStmtInfo(4, StmtType::WhileStmt);
+    shared_ptr<StmtInfo> assign_stmt_5 = TestUtilities::createStmtInfo(5, StmtType::Assign);
+    shared_ptr<StmtInfo> assign_stmt_6 = TestUtilities::createStmtInfo(6, StmtType::Assign);
+    shared_ptr<StmtInfo> while_stmt_7 = TestUtilities::createStmtInfo(7, StmtType::WhileStmt);
+    shared_ptr<StmtInfo> read_stmt_8 = TestUtilities::createStmtInfo(8, StmtType::Read);
+    shared_ptr<StmtInfo> assign_stmt_9 = TestUtilities::createStmtInfo(9, StmtType::Assign);
+    shared_ptr<StmtInfo> assign_stmt_10 = TestUtilities::createStmtInfo(10, StmtType::Assign);
+
+    CHECK_NOTHROW(cfg.createNode(assign_stmt_1));
+    CHECK_NOTHROW(cfg.createNode(while_stmt_2));
+    CHECK_NOTHROW(cfg.createNode(assign_stmt_3));
+    CHECK_NOTHROW(cfg.createNode(while_stmt_4));
+    CHECK_NOTHROW(cfg.createNode(assign_stmt_5));
+    CHECK_NOTHROW(cfg.createNode(assign_stmt_6));
+    CHECK_NOTHROW(cfg.createNode(while_stmt_7));
+    CHECK_NOTHROW(cfg.createNode(read_stmt_8));
+    CHECK_NOTHROW(cfg.createNode(assign_stmt_9));
+    CHECK_NOTHROW(cfg.createNode(assign_stmt_10));
+
+    CHECK_NOTHROW(modifies_store.set(assign_stmt_1, "x"));
+    CHECK_NOTHROW(modifies_store.set(assign_stmt_3, "x"));
+    CHECK_NOTHROW(modifies_store.set(assign_stmt_5, "b"));
+    CHECK_NOTHROW(modifies_store.set(assign_stmt_6, "x"));
+    CHECK_NOTHROW(modifies_store.set(read_stmt_8, "x"));
+    CHECK_NOTHROW(modifies_store.set(assign_stmt_9, "x"));
+    CHECK_NOTHROW(modifies_store.set(assign_stmt_10, "z"));
+
+    CHECK_NOTHROW(uses_store.set(while_stmt_2, "a"));
+    CHECK_NOTHROW(uses_store.set(assign_stmt_3, "x"));
+    CHECK_NOTHROW(uses_store.set(while_stmt_4, "y"));
+    CHECK_NOTHROW(uses_store.set(assign_stmt_5, "x"));
+    CHECK_NOTHROW(uses_store.set(assign_stmt_6, "b"));
+    CHECK_NOTHROW(uses_store.set(while_stmt_7, "a"));
+    CHECK_NOTHROW(uses_store.set(assign_stmt_9, "x"));
+    CHECK_NOTHROW(uses_store.set(assign_stmt_10, "x"));
+
+    CHECK_NOTHROW(next_manager.setNext(1, 2));
+    CHECK_NOTHROW(next_manager.setNext(2, 3));
+    CHECK_NOTHROW(next_manager.setNext(3, 2));
+    CHECK_NOTHROW(next_manager.setNext(2, 4));
+    CHECK_NOTHROW(next_manager.setNext(4, 5));
+    CHECK_NOTHROW(next_manager.setNext(5, 6));
+    CHECK_NOTHROW(next_manager.setNext(6, 7));
+    CHECK_NOTHROW(next_manager.setNext(7, 8));
+    CHECK_NOTHROW(next_manager.setNext(7, 4));
+    CHECK_NOTHROW(next_manager.setNext(8, 9));
+    CHECK_NOTHROW(next_manager.setNext(9, 7));
+    CHECK_NOTHROW(next_manager.setNext(4, 10));
+
+    REQUIRE(affects_manager.checkAffects(1, 3));
+    REQUIRE(affects_manager.checkAffects(1, 5));
+    REQUIRE(affects_manager.checkAffects(1, 10));
+    REQUIRE(affects_manager.checkAffects(3, 3));
+    REQUIRE(affects_manager.checkAffects(3, 5));
+    REQUIRE(affects_manager.checkAffects(3, 10));
+    REQUIRE(affects_manager.checkAffects(5, 6));
+    REQUIRE(affects_manager.checkAffects(6, 5));
+    REQUIRE(affects_manager.checkAffects(6, 10));
+    REQUIRE(affects_manager.checkAffects(9, 5));
+    REQUIRE(affects_manager.checkAffects(9, 10));
+    REQUIRE_THROWS(affects_manager.checkAffects(2, 3));
+    REQUIRE_THROWS(affects_manager.checkAffects(4, 5));
+    REQUIRE_THROWS(affects_manager.checkAffects(7, 8));
+    REQUIRE_THROWS(affects_manager.checkAffects(8, 9));
+    REQUIRE_FALSE(affects_manager.checkAffects(1, 9));
+    REQUIRE_FALSE(affects_manager.checkAffects(3, 1));
+    REQUIRE_FALSE(affects_manager.checkAffects(6, 8));
+    REQUIRE_FALSE(affects_manager.checkAffects(6, 9));
+    REQUIRE_FALSE(affects_manager.checkAffects(9, 9));
+    REQUIRE_FALSE(affects_manager.checkAffects(5, 10));
+}
