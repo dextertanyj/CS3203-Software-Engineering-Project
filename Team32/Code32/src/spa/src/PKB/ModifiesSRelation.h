e815296a5d589ed0715d77171e08e6e413fee003
Wei Kit <weikit89@gmail.com>
2022-03-26 16:03:05 +0800

    Abstract methods in ModifiesSRelation

--- Team32/Code32/src/spa/src/PKB/ModifiesSRelation.h
+++ Team32/Code32/src/spa/src/PKB/ModifiesSRelation.h
@@ -20,6 +20,7 @@ private:
 	                         SVRelationStore<ModifiesSRelation>& store);
 	static void optimizeConditional(const shared_ptr<StmtInfo>& statement, Types::ParentStore& parent_store,
 	                                SVRelationStore<ModifiesSRelation>& store);
+	static void storeModifiedVars(shared_ptr<StmtInfo> stmt_key, StmtInfoPtrSet stmt_list, SVRelationStore<ModifiesSRelation>& store);
 };
 
 #endif  // SPA_SRC_PKB_MODIFIESSRELATION_H
