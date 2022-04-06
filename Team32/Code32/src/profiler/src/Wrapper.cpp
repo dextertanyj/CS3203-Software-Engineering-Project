2a4f172a6f49af84b20ecba0d6ed7cf7ee1b11d8
Dexter Tan <dev@dextertanyj.com>
2022-04-06 19:16:50 +0800

    Update profiler

--- Team32/Code32/src/profiler/src/Wrapper.cpp
+++ Team32/Code32/src/profiler/src/Wrapper.cpp
@@ -11,7 +11,7 @@ void Wrapper::parse(const std::string& filename) {
 	source_processor.process(source);
 }
 
-void Wrapper::evaluate(const std::string& query, list<std::string>& results) {
+void Wrapper::evaluate(const std::string& query, std::list<std::string>& results) {
 	std::vector<std::string> internal_results = query_processor.processQuery(query);
 	results.insert(results.end(), internal_results.begin(), internal_results.end());
 }
