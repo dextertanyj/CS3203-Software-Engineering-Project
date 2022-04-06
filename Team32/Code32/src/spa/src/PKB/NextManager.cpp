f1be979a5b224a47d887dc551f6c2dd068561559
ypinhsuan <yenpinhsuan@gmail.com>
2022-04-06 13:44:58 +0800

    Minor fix

--- Team32/Code32/src/spa/src/PKB/NextManager.cpp
+++ Team32/Code32/src/spa/src/PKB/NextManager.cpp
@@ -39,17 +39,6 @@ StmtInfoPtrSet PKB::NextManager::getNext(StmtRef node_ref) {
 	if (!control_flow_graph.contains(node_ref)) {
 		return {};
 	}
-	shared_ptr<PKB::StatementNode> curr_node = control_flow_graph.getNode(node_ref);
-	StmtInfoPtrSet next_nodes;
-	for (const auto& node : curr_node->getNextNodes()) {
-		if (node->getNodeType() == Types::NodeType::Dummy) {
-			StmtInfoPtrSet next_of_dummy = PKB::ControlFlowGraph::collectNextOfDummy(node);
-			next_nodes.insert(next_of_dummy.begin(), next_of_dummy.end());
-		} else {
-			shared_ptr<PKB::StatementNode> stmt_node = dynamic_pointer_cast<PKB::StatementNode>(node);
-			next_nodes.emplace(stmt_node->getStmtInfo());
-		}
-	}
 	return control_flow_graph.getNextNodes(node_ref);
 }
 
@@ -74,18 +63,6 @@ StmtInfoPtrSet PKB::NextManager::getNextStar(StmtRef node_ref) {
 StmtInfoPtrSet PKB::NextManager::getPrevious(StmtRef node_ref) {
 	if (!control_flow_graph.contains(node_ref)) {
 		return {};
-	}
-	shared_ptr<PKB::StatementNode> curr_node = this->control_flow_graph.getNode(node_ref);
-	StmtInfoPtrSet prev_nodes;
-	for (const auto& node : curr_node->getPreviousNodes()) {
-		// If previous node is a dummy node, need to get the previous nodes of the dummy node.
-		if (node->getNodeType() == Types::NodeType::Dummy) {
-			StmtInfoPtrSet prev_of_dummy = PKB::ControlFlowGraph::collectPreviousOfDummy(node);
-			prev_nodes.insert(prev_of_dummy.begin(), prev_of_dummy.end());
-		} else {
-			shared_ptr<PKB::StatementNode> stmt_node = dynamic_pointer_cast<StatementNode>(node);
-			prev_nodes.emplace(stmt_node->getStmtInfo());
-		}
 	}
 	return control_flow_graph.getPreviousNodes(node_ref);
 }
