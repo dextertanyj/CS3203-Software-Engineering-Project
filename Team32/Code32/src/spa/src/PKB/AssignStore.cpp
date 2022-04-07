57f75d4e935206f073cfa06a44679ce0156cbb4a
Wei Kit <weikit89@gmail.com>
2022-04-07 15:35:03 +0800

    minor fix

--- Team32/Code32/src/spa/src/PKB/AssignStore.cpp
+++ Team32/Code32/src/spa/src/PKB/AssignStore.cpp
@@ -35,7 +35,7 @@ bool PKB::AssignStore::patternExists(const VarRef& variable, const Common::Expre
 		return false;
 	}
 	return any_of(iter->second.begin(), iter->second.end(),
-	              [&](const AssignRelation& ar) { compareExpressions(ar.expression, expression, is_exact_match); });
+	              [&](const AssignRelation& ar) { return compareExpressions(ar.expression, expression, is_exact_match); });
 }
 
 StmtInfoPtrSet PKB::AssignStore::getStmtsWithPattern(const VarRef& variable, const Common::ExpressionProcessor::Expression& expression,
