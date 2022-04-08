b1f4c7f1134487b852702591280ef3633357953b
Dexter Tan <dev@dextertanyj.com>
2022-04-08 13:57:12 +0800

    Cleanup Query Result

--- Team32/Code32/src/spa/src/QP/QueryResult.h
+++ Team32/Code32/src/spa/src/QP/QueryResult.h
@@ -23,21 +23,25 @@ public:
 	explicit QueryResult(bool result);
 	explicit QueryResult(std::vector<std::string> synonyms);
 	explicit QueryResult(ResultTable table);
-	[[nodiscard]] bool getResult() const;
-	[[nodiscard]] bool containsSynonym(const std::string& synonym) const;
-	[[nodiscard]] ResultTable getTable() const;
-	[[nodiscard]] ResultColumn getSynonymResult(const std::string& synonym) const;
-	[[nodiscard]] ResultRow getRowWithOrder(const std::vector<std::string>& synonyms, size_t row_number) const;
-	[[nodiscard]] size_t getNumberOfRows() const;
-	[[nodiscard]] std::vector<std::string> getSynonymsStored() const;
+
 	void addRow(const ResultRow& row);
 	void filterBySelect(const QP::Types::DeclarationList& select_list);
+
+	[[nodiscard]] bool getResult() const;
+	[[nodiscard]] size_t getNumberOfRows() const;
+	[[nodiscard]] bool containsSynonym(const std::string& synonym) const;
+	[[nodiscard]] std::vector<std::string> getSynonymsStored() const;
+	[[nodiscard]] ResultColumn getSynonymResult(const std::string& synonym) const;
+	[[nodiscard]] ResultRow getRowWithOrder(const std::vector<std::string>& synonyms, size_t row_number) const;
+	[[nodiscard]] ResultTable getTable() const;
+
 	static QueryResult joinResults(std::vector<QueryResult>& result);
 	static QueryResult joinIntraGroupResults(std::vector<QueryResult>& result);
 
 private:
 	bool result;
 	ResultTable table;
+
 	static bool compareLength(const QueryResult& lhs, const QueryResult& rhs);
 	static std::unordered_map<std::string, std::vector<size_t>> getSynonymToResultIndexMap(const std::vector<QueryResult>& results);
 	static void findNeighbours(const QueryResult& current, std::unordered_map<std::string, std::vector<size_t>>& synonym_to_index_map,
