1797a7be2f8a2fa7cafc368cc5215e28c4be21b9
Dexter Tan <dev@dextertanyj.com>
2022-04-08 13:37:54 +0800

    Cleanup ClauseArgument

--- Team32/Code32/src/spa/src/QP/ClauseArgument.h
+++ Team32/Code32/src/spa/src/QP/ClauseArgument.h
@@ -10,82 +10,22 @@
 
 class QP::ClauseArgument {
 public:
-	/**
-	 * Constructs a ClauseArgument object with wildcard type.
-	 */
 	explicit ClauseArgument();
-
-	/**
-	 * Constructs a ClauseArgument object with synonym type.
-	 * @param synonym
-	 */
 	explicit ClauseArgument(Types::Declaration synonym);
-
-	/**
-	 * Constructs a ClauseArgument object with synonym and attribute type.
-	 * @param attribute
-	 */
 	explicit ClauseArgument(Types::Attribute attribute);
-
-	/**
-	 * Constructs a ClauseArgument object with synonym type.
-	 * @param name
-	 */
 	explicit ClauseArgument(std::string name);
-
-	/**
-	 * Constructs a ClauseArgument object storing a statement index.
-	 * @param statement_index
-	 */
 	explicit ClauseArgument(StmtRef statement_index);
-
 	explicit ClauseArgument(Common::EP::Expression expression, bool exact);
 
-	/**
-	 * Returns the type of argument stored.
-	 * @return the type of argument stored.
-	 */
 	[[nodiscard]] Types::ArgumentType getType() const;
-
-	/**
-	 * Returns the synonym stored.
-	 * @throws ClauseArgumentException if a synonym is not stored.
-	 * @return the synonym stored.
-	 */
 	[[nodiscard]] Types::Declaration getSynonym() const;
-
 	[[nodiscard]] std::string getSynonymSymbol() const;
 	[[nodiscard]] Types::DesignEntity getSynonymType() const;
-
-	/**
-	 * Returns the synonym and attribute stored.
-	 * @throws ClauseArgumentException if a synonym and attribute are not stored.
-	 * @return the synonym and attribute stored.
-	 */
 	[[nodiscard]] Types::Attribute getAttribute() const;
 	[[nodiscard]] Types::AttributeType getAttributeType() const;
-
-	/**
-	 * Returns the name stored.
-	 * @throws ClauseArgumentException if a name is not stored.
-	 * @return the name stored.
-	 */
 	[[nodiscard]] std::string getName() const;
-
-	/**
-	 * Returns the statement index stored.
-	 * @throws ClauseArgumentException if a statement index is not stored.
-	 * @return the statement index stored.
-	 */
 	[[nodiscard]] StmtRef getStatementIndex() const;
-
-	/**
-	 * Returns the expression stored.
-	 * @throws ClauseArgumentException if an expression is not stored.
-	 * @return the expression stored.
-	 */
 	[[nodiscard]] Common::EP::Expression getExpression() const;
-
 	[[nodiscard]] bool operator==(const ClauseArgument& other) const;
 
 	friend std::hash<ClauseArgument>;
